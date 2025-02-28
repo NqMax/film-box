5e80ead0522c2853d66b8e00553834a75e8cc381
Max <111196993+NqMax@users.noreply.github.com>
2025-02-28 02:48:05 -0600

    feat: add header to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${sourceSans3.className}`}
       >
+        <Header />
         {children}
       </body>
     </html>
